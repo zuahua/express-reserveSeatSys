ffbab937e67d9550d49b8064b01415bfbe85be76
zuahua <1985857412@qq.com>
2020-05-16 14:23:27 +0800

    20200516

---
+++ modules/const/password.js
@@ -0,0 +1,15 @@
+// 存放各类用户名密码
+
+// 邮件地址
+const mailAddress = 'xxxxxx'
+// 邮件密码
+const mailPassword = 'xxxxxx'
+
+// sql密码
+sqlPassword = 'xxxxxx'
+
+module.exports = {
+	mailAddress,
+	mailPassword,
+	sqlPassword
+}
